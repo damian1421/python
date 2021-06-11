6611e641306feef47b08269b4f6d5463838dbd82
damian1421 <damian.e.salvador@gmail.com>
2021-06-11 06:33:37 -0300

    [New] Created tables.py & Initialized radios.py (In construction...)

---
+++ radios.py
@@ -0,0 +1,11 @@
+# Author: l0gg3r
+# Source: https://github.com/damian1421/python/radios.py
+
+print("Radios", "by l0gg3r")
+#Here goes a menu that iterates between numbers of items and lines of another file with urls.
+# 1- Vale 97.5 https://vale97.5/online.m3u
+print("1- FM 97.5	- Vale 97.5")
+print("2- FM 99.9	- La 100")
+print("3- FM 104.3	- Urbana Play")
+choice = input("Choose a number...")
+print("Loading stream: ",choice)
---
+++ tables.py
@@ -0,0 +1,6 @@
+numbers = range(1,11)
+for x in numbers:
+	print ("Table of: ",x)
+	for i in range(1,11):
+		print (x," x ",i," = ",x * i)
+	print ("------------------------------")
