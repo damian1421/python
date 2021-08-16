86d245fe980f1186088cf2ef68aead25463b450f
damian1421 <damian.e.salvador@gmail.com>
2021-08-16 08:56:18 -0300

    [NEW] CCTV Storage Calculator: calculate needed storage for CCTV Systems & available days from your capacity

--- cctv-storage-calculator.py
+++ cctv-storage-calculator.py
@@ -1,6 +1,6 @@
 #Description: Utility to caculate storage needed for CCTV storage.
 #Author: l0gg3r
-#Date: 2021-08-10
+#Date: 2021-08-16
 import os
 
 clear = lambda: os.system('clear') #Define 'clear' command
@@ -12,34 +12,51 @@ try:
     print ("CCTV Storage Calculator") #Greetings
 
     #Complete with the requested info to start the calculation
+    server = input('Server: ')
     cameras = int(input('Number of cameras: '))
-    VBR = input('Is VBR? Yes-No: ')
-    if VBR == 'y' or VBR == 'Y':
-        cam_min_bitrate = int(input('Min Kbps (Kilobits x second) x camera: '))
-        quality = int(input('Quality 1-10(10 is the best): '))
-    cam_max_bitrate = int(input('Max Kbps (Kilobits x second) x camera: '))
+
+    VBR = input('Is VBR? Yes-No: ') #If VBR == 'yes' then set adaptative bitrate
+    if 'y' in VBR or 'Y' in VBR:
+        cam_min_bitrate = int(input('Min Kbps (Kilobits x second) by camera: '))
+        quality = int(input('Quality 1-10 (10 is the best): '))
+    
+    cam_max_bitrate = int(input('Max Kbps (Kilobits x second) by camera: '))
     retention_days = int(input('Retention days: '))
-    retention_TB = int(cameras #Number of cameras
-                       * cam_max_bitrate / 8 #Max bitrate used by each camera KBps
-                       * 3600 #KBph in 1 hour
-                       * 24 / 1024 #MBpd in 1 day
-                       * retention_days / 1024 / float(1024) #Total retention days TB
-                       )
-    if VBR == 'n' or VBR == 'N': #Apply for CBR
-        print (f"Storage needed for {cameras} cameras: {retention_TB} TB")
-    else: #Apply for VBR
-        min_retention_TB = int(cameras #Number of cameras
-                       * cam_min_bitrate / 8 #Max bitrate used by each camera KBps
-                       * 3600 #KBph in 1 hour
-                       * 24 / 1024 #MBpd in 1 day
-                       * retention_days / 1024 / 1024 #TB in total retention period
-                       )
-        VBR_adaptative = retention_TB - min_retention_TB # Search variation bitrate_min_to_max
-        VBR_adaptative = VBR_adaptative * quality #This and next_row will set the percentage of the quality required over the prev_row
-        VBR_adaptative = VBR_adaptative / float(10)
+    
+    #Max TB needed for retention period
+    max_retention_TB = float(cameras #Number of cameras
+                            * cam_max_bitrate / 8 #Max KBps used by camera
+                            * 86400 # KB in a day (KB per day = KBps * 60 * 60 * 24 )
+                            / 1073741824 # Convert KB to TB in a day (TB = KB / 1024 / 1024 /1024 )
+                            * retention_days
+                            )
+
+    #Print results for VBR
+    if 'y' in VBR or 'Y' in VBR:
+        #Min TB needed for retention period
+        min_retention_TB = float(cameras #Number of cameras
+                                * cam_min_bitrate / 8 #Min KBps used by camera
+                                * 86400 # KB in a day (KB per day = KBps * 60 * 60 * 24 )
+                                / 1073741824 # Convert KB to TB in a day (TB = KB / 1024 / 1024 /1024 )
+                                * retention_days
+                                )
+        # Calculate average of VBR
+        VBR_average = ( (max_retention_TB - min_retention_TB) * (quality / float(10)) + min_retention_TB)
+
         print (f"Storage needed for {cameras} cameras: {nl}\
-              Min: {min_retention_TB} TB {nl}\
-              Max: {retention_TB} TB")
-        print ("Will try to reach average of: ",min_retention_TB + VBR_adaptative, " TB")
-except:
-    print("Something has failed. Review code.")
+               Min: {min_retention_TB:.2f} TB {nl}\
+               Max: {max_retention_TB:.2f} TB")
+        print (f"Will try to reach average of: {VBR_average:.2f} TB")
+    else: #Print results for CBR
+        print (f"Storage needed for {cameras} cameras: {max_retention_TB:.2f} TB")
+
+    #Reverse calculation
+    reverse_calculation = input('Would you like to check how many days you can store? Yes-No: ')
+    if "y" in reverse_calculation or "Y" in reverse_calculation:
+        how_many_TB = int(input("How many TB is in your storage?: "))
+        how_many_days = int(how_many_TB / ((cameras * (cam_max_bitrate / 8 ))* 86400 / 1073741824))
+        print(f"{how_many_TB} TB hold recordings of {cameras} cameras for {how_many_days} days.")
+except Exception as error:
+    print(f"Something has failed. Review code for {error}.")
+finally:
+    print (f"{nl} Finished")
